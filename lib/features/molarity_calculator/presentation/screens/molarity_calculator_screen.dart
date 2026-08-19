90b65da5b4f2341f63101698eef9e239c769a616
Khansajid55 <sajidkhanin945@gmail.com>
2026-08-19 22:51:53 +0530

    Pushing at 10:51 pm

--- lib/features/molarity_calculator/presentation/screens/molarity_calculator_screen.dart
+++ lib/features/molarity_calculator/presentation/screens/molarity_calculator_screen.dart
@@ -389,7 +389,6 @@ class _MolarityCalculatorScreenState extends State<MolarityCalculatorScreen> {
                       ),
                     ],
                   ],
-                  ],
                 ),
               ),
             );
